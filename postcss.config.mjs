f187b2a474fae8a49fc7f1798273785eeada9a41
Mostafa Deiab <dev@mostafadeiab.com>
2025-03-21 00:40:24 -0400

    Fixing dependency issues

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/postcss': {}, // Use this instead of tailwindcss: {}
     autoprefixer: {},
   },
 };
