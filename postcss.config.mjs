9293283d245439849f0f5ccbc92a2751d817a384
Mostafa Deiab <dev@mostafadeiab.com>
2025-06-21 11:50:54 -0400

    Potential Fix for TailwindCSS

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    '@tailwindcss/postcss': {}, // Use this instead of tailwindcss: {}
+    tailwindcss: {}, // Use 'tailwindcss', not '@tailwindcss/postcss'
     autoprefixer: {},
   },
 };
