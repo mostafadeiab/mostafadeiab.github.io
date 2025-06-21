7055aa4fa648e36943e7076fb75e8ddc0c6571d9
Mostafa Deiab <dev@mostafadeiab.com>
2025-06-21 11:59:17 -0400

    Add @tailwindcss/postcss dependency to fix build

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {}, // Use 'tailwindcss', not '@tailwindcss/postcss'
+    '@tailwindcss/postcss': {},
     autoprefixer: {},
   },
 };
